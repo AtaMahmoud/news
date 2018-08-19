247f892ab8027e7d329a270c1fd83a54679cd7ae
Ata Mahmoud <ata.ata250@hotmail.com>
2018-08-20 01:06:44 +0200

    implement ScanStreamTransformer

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,20 +1,30 @@
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
+import 'dart:async';
 
-class StoriesBloc{
-  final _repository=new Repository();
-  final _toIdsController=new PublishSubject<List<int>>();
+class StoriesBloc {
+  final _repository = new Repository();
+  final _toIdsController = new PublishSubject<List<int>>();
 
-  Observable<List<int>> get topIdsStream=>_toIdsController.stream;
+  Observable<List<int>> get topIdsStream => _toIdsController.stream;
 
-  fetchTopIds()async{
-    final ids=await _repository.fetchTopIds();
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
     _toIdsController.sink.add(ids);
   }
 
-  dispose(){
-    _toIdsController.close();
+  _itemTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id]=_repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
   }
 
+  dispose() {
+    _toIdsController.close();
+  }
 }
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,15 +1,35 @@
 import 'dart:async';
-
+import '../blocs/stories_provider.dart';
 import 'package:flutter/material.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final storiesBloc = StoriesProvider.of(context);
+    storiesBloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Top News'),
+        title: Text('News'),
       ),
-      body: Text('Show List Here!'),
+      body: buildList(storiesBloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc storiesBloc) {
+    return StreamBuilder(
+      stream: storiesBloc.topIdsStream,
+      builder: (context,AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, int index) {
+              return Text('${snapshot.data[index]}');
+            },);
+      },
     );
   }
 }
