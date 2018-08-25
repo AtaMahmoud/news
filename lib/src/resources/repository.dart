39cc1e9f63af996f4fe5c632e71e50a887f80b2a
Ata Mahmoud <ata.ata250@hotmail.com>
2018-08-25 22:21:13 +0200

    refactor the repository to solve the bug

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,48 +1,57 @@
-import 'news_api_provider.dart';
-import 'news_db_provider.dart';
 import 'dart:async';
+
+import 'news_api_provider.dart';
+
+import 'news_db_provider.dart';
+
 import '../models/item_model.dart';
 
-class Repository{
-
-  List<Source> sources=<Source>[
+class Repository {
+  List<Source> sources = <Source>[
     newsDbProvider,
-    new NewsApiProvider()
+    NewsApiProvider(),
   ];
 
-  List<Cache> caches=<Cache>[
-    newsDbProvider
+  List<Cache> caches = <Cache>[
+    newsDbProvider,
   ];
 
-  Future<List<int>> fetchTopIds(){
-    //TODO : iterate over sources when dbprovider implemented it
+  // Iterate over sources when dbprovider
+
+  // get fetchTopIds implemented
+
+  Future<List<int>> fetchTopIds() {
     return sources[1].fetchTopIds();
   }
 
-  Future<ItemModel> fetchItem(int id) async{
+  Future<ItemModel> fetchItem(int id) async {
     ItemModel item;
-    Source source;
+    var source;
 
-    for(source in sources){
-      item= await source.fetchItem(id);
-      if(item!=null){
+    for (source in sources) {
+      item = await source.fetchItem(id);
+
+      if (item != null) {
         break;
       }
+    }
 
-      for(var cache in caches){
+    for (var cache in caches) {
+      if (cache != source) {
         cache.addItem(item);
       }
-
-      return item;
     }
+
+    return item;
   }
 }
 
-abstract class Source{
+abstract class Source {
   Future<List<int>> fetchTopIds();
+
   Future<ItemModel> fetchItem(int id);
 }
 
-abstract class Cache{
+abstract class Cache {
   Future<int> addItem(ItemModel item);
 }
